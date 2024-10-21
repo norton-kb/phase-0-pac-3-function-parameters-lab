49434668ec8869d97b55433039dea92cf00c68bc
norton-kb <biinorton080@gmail.com>
2024-10-21 21:23:10 +0300

    parameters lab

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(Aki) {
+    return "Hi, my name is " + Aki + ".";
+}
+function introductionWithLanguage(Aki, Ember_js) {
+    return "Hi, my name is " + Aki + " and I am learning to program in " + Ember_js + ".";
+}
+function introductionWithLanguageOptional(name, langauge = "JavaScript") {
+    return "Hi, my name is " + name + " and I am learning to program in " + langauge + ".";
+}
